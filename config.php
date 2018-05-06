3a5c34343a711956afeb651f425984a6b626d56f
darkelement1987 <jgernaat1987@gmail.com>
2018-05-07 00:36:31 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,1 +1,16 @@
+<?php
+$servername = "localhost";
+$username = "user";
+$password = "password";
+$database = "database";
 
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>

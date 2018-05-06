4c8338d15f3d6411c876d21eb4227b0653797cfc
darkelement1987 <jgernaat1987@gmail.com>
2018-05-07 00:19:39 +0200

    Add scan location button

--- index.php
+++ index.php
@@ -1,25 +1,27 @@
 <h2><strong>Add spot:</strong></h2>
-<table cellspacing="5" cellpadding="5">
+<table style="width: 430px;" cellspacing="5" cellpadding="5">
 <tbody>
 <tr>
 <td style="width: 118px;">Pokemon</td>
-<td style="width: 237px;"><input type="text" /></td>
+<td style="width: 273px;"><input type="text" /></td>
 </tr>
 <tr>
 <td style="width: 118px;">CP</td>
-<td style="width: 237px;"><input type="text" /></td>
+<td style="width: 273px;"><input type="text" /></td>
 </tr>
 <tr>
 <td style="width: 118px;">Time</td>
-<td style="width: 237px;"><input maxlength="2" size="2" type="text" />:<input maxlength="2" size="2" type="text" />&nbsp; (HH:MM)</td>
+<td style="width: 273px;"><input maxlength="2" size="2" type="text" />:<input maxlength="2" size="2" type="text" />&nbsp; (HH:MM)</td>
 </tr>
 <tr>
-<td style="width: 118px;">Adress</td>
-<td style="width: 237px;"><input type="text" /></td>
+<td style="width: 118px;">Address</td>
+<td style="width: 273px;">
+<p><input type="text" /><input type="submit" value="Scan location" /></p>
+</td>
 </tr>
 <tr>
 <td style="width: 118px;">Submit</td>
-<td style="width: 237px;"><input type="submit" value="Add spot" /></td>
+<td style="width: 273px;"><input type="submit" value="Add spot" /></td>
 </tr>
 </tbody>
 </table>
@@ -33,10 +35,10 @@
 <td style="width: 169px;"><strong>Address</strong></td>
 </tr>
 <tr>
-<td style="width: 168px;">&nbsp;</td>
-<td style="width: 169px;">&nbsp;</td>
-<td style="width: 169px;">&nbsp;</td>
-<td style="width: 169px;">&nbsp;</td>
+<td style="width: 168px;">&nbsp;<img src="https://github.com/darkelement1987/spotpokemon/blob/master/icons/1.png?raw=true" alt="1.png" width="32" height="32" /></td>
+<td style="width: 169px;">&nbsp;999</td>
+<td style="width: 169px;">&nbsp;12:00</td>
+<td style="width: 169px;">Main Street 1</td>
 </tr>
 <tr>
 <td style="width: 168px;">&nbsp;</td>

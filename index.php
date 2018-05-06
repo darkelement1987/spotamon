d31e6603221e4da24c0556f5747b91e969dd61c2
darkelement1987 <jgernaat1987@gmail.com>
2018-05-06 23:18:58 +0200

    redesign

--- index.php
+++ index.php
@@ -1,104 +1,96 @@
-<table width="343">
+<h2><strong>Add spot:</strong></h2>
+<table style="height: 123px;" width="190">
 <tbody>
 <tr>
-<td>
-<p>Pokemon name</p>
-<p><input type="text" /></p>
-</td>
-<td>
-<p>Spot time:</p>
-<p><input type="text" /></p>
-</td>
-<td>
-<p>CP:</p>
-<p><input type="text" /></p>
-</td>
-<td>
-<p>Address:</p>
-<p><input type="text" /></p>
-</td>
-<td>
-<p>Submit</p>
-<p><input type="submit" value="Submit" /></p>
-</td>
+<td style="width: 87px;">Pokemon</td>
+<td style="width: 87px;"><input type="text" /></td>
 </tr>
 <tr>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
+<td style="width: 87px;">CP</td>
+<td style="width: 87px;"><input type="text" /></td>
 </tr>
 <tr>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
+<td style="width: 87px;">Time</td>
+<td style="width: 87px;"><input type="text" /></td>
 </tr>
 <tr>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
+<td style="width: 87px;">Adress</td>
+<td style="width: 87px;"><input type="text" /></td>
 </tr>
 <tr>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-</tr>
-<tr>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-</tr>
-<tr>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-</tr>
-<tr>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-</tr>
-<tr>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-</tr>
-<tr>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-</tr>
-<tr>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-</tr>
-<tr>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
+<td style="width: 87px;">Submit</td>
+<td style="width: 87px;"><input type="submit" value="Add spot" /></td>
 </tr>
 </tbody>
 </table>
-<p style="text-align: center;">Page # of #</p>
+<h2><strong>Spotted Pokemon:</strong></h2>
+<table style="height: 238px;" width="703">
+<tbody>
+<tr>
+<td style="width: 168px;"><strong>Pokemon</strong></td>
+<td style="width: 169px;"><strong>CP</strong></td>
+<td style="width: 169px;"><strong>Time</strong></td>
+<td style="width: 169px;"><strong>Address</strong></td>
+</tr>
+<tr>
+<td style="width: 168px;">&nbsp;</td>
+<td style="width: 169px;">&nbsp;</td>
+<td style="width: 169px;">&nbsp;</td>
+<td style="width: 169px;">&nbsp;</td>
+</tr>
+<tr>
+<td style="width: 168px;">&nbsp;</td>
+<td style="width: 169px;">&nbsp;</td>
+<td style="width: 169px;">&nbsp;</td>
+<td style="width: 169px;">&nbsp;</td>
+</tr>
+<tr>
+<td style="width: 168px;">&nbsp;</td>
+<td style="width: 169px;">&nbsp;</td>
+<td style="width: 169px;">&nbsp;</td>
+<td style="width: 169px;">&nbsp;</td>
+</tr>
+<tr>
+<td style="width: 168px;">&nbsp;</td>
+<td style="width: 169px;">&nbsp;</td>
+<td style="width: 169px;">&nbsp;</td>
+<td style="width: 169px;">&nbsp;</td>
+</tr>
+<tr>
+<td style="width: 168px;">&nbsp;</td>
+<td style="width: 169px;">&nbsp;</td>
+<td style="width: 169px;">&nbsp;</td>
+<td style="width: 169px;">&nbsp;</td>
+</tr>
+<tr>
+<td style="width: 168px;">&nbsp;</td>
+<td style="width: 169px;">&nbsp;</td>
+<td style="width: 169px;">&nbsp;</td>
+<td style="width: 169px;">&nbsp;</td>
+</tr>
+<tr>
+<td style="width: 168px;">&nbsp;</td>
+<td style="width: 169px;">&nbsp;</td>
+<td style="width: 169px;">&nbsp;</td>
+<td style="width: 169px;">&nbsp;</td>
+</tr>
+<tr>
+<td style="width: 168px;">&nbsp;</td>
+<td style="width: 169px;">&nbsp;</td>
+<td style="width: 169px;">&nbsp;</td>
+<td style="width: 169px;">&nbsp;</td>
+</tr>
+<tr>
+<td style="width: 168px;">&nbsp;</td>
+<td style="width: 169px;">&nbsp;</td>
+<td style="width: 169px;">&nbsp;</td>
+<td style="width: 169px;">&nbsp;</td>
+</tr>
+<tr>
+<td style="width: 168px;">&nbsp;</td>
+<td style="width: 169px;">&nbsp;</td>
+<td style="width: 169px;">&nbsp;</td>
+<td style="width: 169px;">&nbsp;</td>
+</tr>
+</tbody>
+</table>

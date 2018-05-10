7a461101132b6f3dae48a1e9dbafc434fdfd90c8
darkelement1987 <jgernaat1987@gmail.com>
2018-05-10 17:29:10 +0200

    Fix path

--- frontend/functions.php
+++ frontend/functions.php
@@ -2,7 +2,7 @@
 function formsubmission(){
 $pokemon = $monid = $cp = $hour = $min = $ampm = $monster = $dexentry = $latitude = $longitude = $address ="";?>
 <h2 style="text-align:center;"><strong>Add spot:</strong></h2>
-<form id="usersubmit" method="post" action="spot.php">
+<form id="usersubmit" method="post" action="frontend/spot.php">
 <center><table style="width: 20%; height: auto;" cellspacing="5" cellpadding="5">
 <tbody>
 

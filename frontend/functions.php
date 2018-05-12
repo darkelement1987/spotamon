e0b8fce1ee1ffc532a6bf05e52d8fa085d9f3668
darkelement1987 <jgernaat1987@gmail.com>
2018-05-12 03:48:18 +0200

    Disable 'ctrl+scroll' to zoom message

--- frontend/functions.php
+++ frontend/functions.php
@@ -278,7 +278,8 @@ var customLabel = {
   function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     center: new google.maps.LatLng(<?php echo $mapcenter;?>),
-    zoom: 15
+    zoom: 15,
+    gestureHandling: 'greedy'
   });
   var infoWindow = new google.maps.InfoWindow;
 

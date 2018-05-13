c1f11503b4a2ba814e6410fdd9525b2b6e0b1003
darkelement1987 <jgernaat1987@gmail.com>
2018-05-13 17:09:55 +0200

    Add javascript

--- submit-raid.php
+++ submit-raid.php
@@ -9,6 +9,12 @@ include 'config/dbbuilding.php';
 <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
 <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script type="text/javascript" src="//code.jquery.com/jquery-1.8.3.js"></script>        
+<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
+    <script>
+        $(document).ready(function() { $("#raidsearch").select2(); });
+    </script>
 </head>
 
 <?php

53c13e0b9d323984e4c9345ef5216c2514c8bc44
darkelement1987 <jgernaat1987@gmail.com>
2018-05-14 22:44:32 +0200

    Update spot.php

--- spot.php
+++ spot.php
@@ -1,5 +1,4 @@
 <?php
-
 ob_start();
 require 'config/config.php';
 include'functions.php';
@@ -11,16 +10,30 @@ $ampm = $_POST['ampm'];
 $latitude = $_POST['latitude'];
 $longitude = $_POST['longitude'];
 
-$sql = "INSERT INTO spots (pokemon, cp, hour, min, ampm, latitude, longitude) VALUES ('$pokemon','$cp','$hour','$min','$ampm','$latitude','$longitude')";
-	if(!mysqli_query($conn,$sql))
-		{
-			echo 'Not Inserted';
-		}
-			else
-			{
-				echo 'Inserted';
-			}	
 
-	header('Location:index.php');
-	
+$url  = "https://maps.googleapis.com/maps/api/geocode/json?latlng=".$latitude.",".$longitude."&sensor=false&key=".$gmaps;
+$json = @file_get_contents($url);
+$data = json_decode($json);
+$status = $data->status;
+$address = '';
+if($status == "OK")
+{
+    $address = $data->results[0]->formatted_address;
+}
+else
+{
+    $address = "Cannot retrieve address";
+}
+echo $address;
+$sql = "INSERT INTO spots (pokemon, cp, hour, min, ampm, latitude, longitude, fulladdress) VALUES ('$pokemon','$cp','$hour','$min','$ampm','$latitude','$longitude', '$address')";
+if(!mysqli_query($conn,$sql))
+{
+    echo 'Not Inserted';
+}
+else
+{
+    echo 'Inserted';
+}    
+header('Location:index.php');
+    
 ?>

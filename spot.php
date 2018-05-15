c0450fc929040a25a5379951b87973480a02e080
darkelement1987 <jgernaat1987@gmail.com>
2018-05-15 16:02:49 +0200

    Store/Pull address into/from database (Rebase) (#15)
    * Update functions.php
    
    * Update spot.php
    
    * Update config.php
    
    * Update config.php

--- spot.php
+++ spot.php
@@ -1,26 +1,37 @@
 <?php
-
 ob_start();
 require 'config/config.php';
 include'functions.php';
-$pokemon = $_POST['pokemon'];
-$cp = $_POST['cp'];
-$hour = $_POST['hour'];
-$min = $_POST['min'];
-$ampm = $_POST['ampm'];
-$latitude = $_POST['latitude'];
-$longitude = $_POST['longitude'];
-
-$sql = "INSERT INTO spots (pokemon, cp, hour, min, ampm, latitude, longitude) VALUES ('$pokemon','$cp','$hour','$min','$ampm','$latitude','$longitude')";
-	if(!mysqli_query($conn,$sql))
-		{
-			echo 'Not Inserted';
-		}
-			else
-			{
-				echo 'Inserted';
-			}	
-
-	header('Location:index.php');
-	
+$pokemon = $conn->real_escape_string($_POST['pokemon']);
+$cp = $conn->real_escape_string($_POST['cp']);
+$hour = $conn->real_escape_string($_POST['hour']);
+$min = $conn->real_escape_string($_POST['min']);
+$ampm = $conn->real_escape_string($_POST['ampm']);
+$latitude = $conn->real_escape_string($_POST['latitude']);
+$longitude = $conn->real_escape_string($_POST['longitude']);
+$url  = "https://maps.googleapis.com/maps/api/geocode/json?latlng=".$latitude.",".$longitude."&sensor=false&key=".$gmaps;
+$json = @file_get_contents($url);
+$data = json_decode($json);
+$status = $data->status;
+$address = '';
+if($status == "OK")
+{
+    $address = $conn->real_escape_string($data->results[0]->formatted_address);
+}
+else
+{
+    $address = "Cannot retrieve address";
+}
+echo $address;
+$sql = "INSERT INTO spots (pokemon, cp, hour, min, ampm, latitude, longitude, fulladdress) VALUES ('$pokemon','$cp','$hour','$min','$ampm','$latitude','$longitude', '$address')";
+if(!mysqli_query($conn,$sql))
+{
+    echo 'Not Inserted';
+}
+else
+{
+    echo 'Inserted';
+}    
+header('Location:index.php');
+    
 ?>

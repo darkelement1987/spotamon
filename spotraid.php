a7ce9d37a9c61c705c85db2a832ee83bc477494b
darkelement1987 <jgernaat1987@gmail.com>
2018-05-16 23:40:20 +0200

    Replace addresses by gymnames on spotted raid page (#17)
    * Replace Addresses by gymnames on spotted raid page
    
    * Update spotraid.php
    
    * Update config.php
    
    * Update spotraid.php

--- spotraid.php
+++ spotraid.php
@@ -9,25 +9,31 @@ $rmin = $conn->real_escape_string($_POST['rmin']);
 $rampm = $conn->real_escape_string($_POST['rampm']);
 $gname = $conn->real_escape_string($_POST['gname']);
 
-$sql = "INSERT INTO spotraid (rboss, rhour, rmin, rampm) VALUES ('$rboss','$rhour','$rmin','$rampm')";
-	if(!mysqli_query($conn,$sql))
-		{
-			echo 'Not Inserted';
-		}
-			else
-			{
-				echo 'Inserted';
-			}
-$sql1 = "UPDATE gyms SET actraid='1',actboss='$rboss' WHERE gid='$gname'";
-	if(!mysqli_query($conn,$sql1))
-		{
-			echo 'Not Inserted';
-		}
-			else
-			{
-				echo 'Inserted';
-			}				
+$query = "SELECT glatitude, glongitude FROM gyms WHERE gid='$gname'";
+$result = mysqli_query($conn,$query)or die(mysqli_error($conn));
+$rows = mysqli_fetch_assoc($result);
+$rlat=$rows["glatitude"];
+$rlon=$rows["glongitude"];
 
-	header('Location:submit-raid.php');
-	
+$sql = "INSERT INTO spotraid (rboss, rhour, rmin, rampm, rlatitude, rlongitude) VALUES ('$rboss','$rhour','$rmin','$rampm','$rlat','$rlon')";
+    if(!mysqli_query($conn,$sql))
+        {
+            echo 'Not Inserted';
+        }
+            else
+            {
+                echo 'Inserted';
+            }
+$sql1 = "UPDATE gyms SET actraid='1',actboss='$rboss' WHERE gid='$gname'";
+    if(!mysqli_query($conn,$sql1))
+        {
+            echo 'Not Inserted';
+        }
+            else
+            {
+                echo 'Inserted';
+            }                
+
+    header('Location:submit-raid.php');
+    
 ?>

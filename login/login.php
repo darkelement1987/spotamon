5dd1eeb03d215d929cb7d808b1594330b3e00177
darkelement1987 <jgernaat1987@gmail.com>
2018-05-23 08:25:57 +0200

    Update login.php

--- login/login.php
+++ login/login.php
@@ -24,7 +24,7 @@
         if($rows==1){
 			$_SESSION['uname'] = $uname;
 			$_SESSION['email'] = $email;
-			 echo "<meta http-equiv=\"refresh\" content=\"0;URL=/index.php\">";
+			 echo "<meta http-equiv=\"refresh\" content=\"0;URL=/login/index.php\">";
             }else{
 				echo "<div class='form'><h3>Username/password is incorrect.</h3><br/>Click here to <a href='login.php'>Login</a></div>";
 				}

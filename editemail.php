154589a397145fed5e58a3890cfb1b4dc5de935f
lilcezz <35689207+lilcezz@users.noreply.github.com>
2018-05-24 22:02:45 -0400

    Update editemail.php

--- editemail.php
+++ editemail.php
@@ -4,6 +4,7 @@ include'frontend/functions.php';
 include'login/auth.php';
 $email = $conn->real_escape_string($_POST['email']);
 // attempt insert query execution
+if(!empty($uname)){
 $sql = "UPDATE users SET email='$email' WHERE uname='".$_SESSION['uname']."'";
 if(mysqli_query($conn, $sql)){
     echo "Records added successfully.";
@@ -14,4 +15,10 @@ if(mysqli_query($conn, $sql)){
 // close connection
 mysqli_close($conn);
 echo "<meta http-equiv=\"refresh\" content=\"0;URL=./profile.php\">";
+} else {
+echo "<br /><center><img src='/static/img/oops2.png'></center>";
+echo "<br /><center>Can not insert a blank email</center>";
+echo "<br /><center>You will be redirected back to <a href='/edit-profile.php'>Edit Profile</a></center>";
+echo "<meta http-equiv='refresh' content='3;url=/edit-profile.php'>";	
+}
 ?>

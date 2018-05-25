df1586cc5d8cfd60e3bbcb2f70bc51bb359c86ba
lilcezz <35689207+lilcezz@users.noreply.github.com>
2018-05-24 22:04:04 -0400

    Update editpassword.php

--- editpassword.php
+++ editpassword.php
@@ -4,6 +4,7 @@ include'frontend/functions.php';
 include'login/auth.php';
 $upass = $conn->real_escape_string($_POST['upass']);
 // attempt insert query execution
+if(!empty($upass)){
 $sql = "UPDATE users SET upass='".md5($upass)."' WHERE uname='".$_SESSION['uname']."'";
 if(mysqli_query($conn, $sql)){
     echo "Records added successfully.";
@@ -14,4 +15,10 @@ if(mysqli_query($conn, $sql)){
 // close connection
 mysqli_close($conn);
 echo "<meta http-equiv=\"refresh\" content=\"0;URL=./profile.php\">";
+} else {
+echo "<br /><center><img src='/static/img/oops2.png'></center>";
+echo "<br /><center>Can not insert a blank Password</center>";
+echo "<br /><center>You will be redirected back to <a href='/edit-profile.php'>Edit Profile</a></center>";
+echo "<meta http-equiv='refresh' content='3;url=/edit-profile.php'>";	
+}
 ?>

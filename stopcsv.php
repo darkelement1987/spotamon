df63dd56404d8f5e7c7e6a7a4a1aa02ce202b2ad
m3g4mInD <fighters_88@hotmail.com>
2018-06-02 13:33:51 +0200

    Remove spaces

--- stopcsv.php
+++ stopcsv.php
@@ -11,7 +11,6 @@ include 'frontend/menu.php';
 </head>
 <?php
 menu();
-
 if(isset($_SESSION["uname"])){
 $result = $conn->query("SELECT * FROM users,usergroup WHERE uname='".$_SESSION['uname']."' AND users.usergroup = usergroup.id LIMIT 1  ");
 $id = $usergroup = "";
@@ -20,26 +19,19 @@ $usergroup = $row['groupname'];
 if ("$usergroup" == 'admin'){
 // PRINTS DATE IN SQL FORMAT \\
 $date = date("Y-m-d H:i:s");
-
     // START CHECK FOR CSV \\
 	if(isset($_POST['submit'])){
         if($_FILES['csv_data']['name']){
-            
             $arrFileName = explode('.',$_FILES['csv_data']['name']);
             if($arrFileName[1] == 'csv'){
                 $handle = fopen($_FILES['csv_data']['tmp_name'], "r");
 				// END CHECK FOR CSV \\
-				
-				
                 while (($data = fgetcsv($handle, 1500, ",")) !== FALSE) { // <-- START LOOP THROUGH CSV FILE \\
-
                     $item1 = mysqli_real_escape_string($conn,$data[0]); // PUT CSV ROW 1 CONTENT IN VAR
                     $item2 = mysqli_real_escape_string($conn,$data[1]); // PUT CSV ROW 2 CONTENT IN VAR
                     $item3 = mysqli_real_escape_string($conn,$data[2]); // PUT CSV ROW 2 CONTENT IN VAR
-					
                     $import="INSERT IGNORE INTO stops (sid, sname, slatitude,slongitude,quested,quest,reward,lured,type,date) VALUES (DEFAULT,'$item1','$item2','$item3',NULL,NULL,NULL,NULL,NULL,'$date')";
                     mysqli_query($conn,$import);
-					
                 } // <<-- END LOOP THROUGH CSV \\
                 fclose($handle);
                 print "<center><h1><b>CSV Loaded!</b></h1></center>";
@@ -52,7 +44,6 @@ $date = date("Y-m-d H:i:s");
             Upload STOP CSV: <input type='file' name='csv_data' /> <input type='submit' name='submit' value='Upload CSV' />
         </form><br></center>
 		</body>
-
 <footer></footer>
 <?php } else{ echo "Sorry you must be an ADMIN to upload this";}} } else {
 	echo "<center><div style='margin-top:5%;'>";

67c27fe3054f407a1b908ec3de90f5f27d72fe2f
darkelement1987 <jgernaat1987@gmail.com>
2018-06-06 21:43:51 +0200

    Add files via upload

---
+++ frontend/questcron.php
@@ -0,0 +1,13 @@
+<?php
+require '../config/config.php';
+$sql = "UPDATE stops SET quested='0',actquest='0',actreward='0',hour='0', min='0',ampm='0',questby='0' WHERE date < (NOW() - INTERVAL 24 HOUR)";
+if(!mysqli_query($conn,$sql))
+{
+    echo 'Not Deleted';
+}
+else
+{
+    echo 'Deleted';
+}        
+ // ends *_query() call
+?>

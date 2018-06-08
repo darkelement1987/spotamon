486aaeb54ce627f1091d67fc7414560dd0e60868
AboveColin <colindv2000@gmail.com>
2018-06-08 18:28:39 +0200

    add Title (#52)

--- index.php
+++ index.php
@@ -5,11 +5,12 @@ include './config/dbbuilding.php';
 ?>
 
 <head>
-<link rel="stylesheet" type="text/css" href="style.css">
-<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
-<meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<script src="https://cdn.klokantech.com/maptilerlayer/v1/index.js"></script>
+	<title>Spotamon</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src="https://cdn.klokantech.com/maptilerlayer/v1/index.js"></script>
 <script>
 
 function submitInstinct(){

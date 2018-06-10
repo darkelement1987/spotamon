bdb2068909aa154a4d55bbd35540d8f80e381b73
darkelement1987 <jgernaat1987@gmail.com>
2018-06-10 03:15:42 +0200

    Add files via upload

--- frontend/menu.php
+++ frontend/menu.php
@@ -2,129 +2,10 @@
 include("login/auth.php");
 require 'config/config.php';
 ?>
-
-	<title>Spotamon</title>
-	<link rel="icon" href="static/img/favicon.ico" type="image/ico" sizes="16x16">
-
-<style>
-body {
-	margin:0;
-	font-family:Arial;
-	height:100%;
-
-	}
-
-.topnav {
-  overflow: hidden;
-  background-color: #333;
-  margin-left: 100px;
-}
-
-.topnav a {
-  float: left;
-  display: block;
-  color: #f2f2f2;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  font-size: 17px;
-}
-
-.active {
-  background-color: inherit;
-  color: white;
-}
-
-.topnav .icon {
-  display: none;
-}
-
-.dropdown {
-    float: left;
-    overflow: hidden;
-}
-
-.dropdown .dropbtn {
-    font-size: 17px;    
-    border: none;
-    outline: none;
-    color: white;
-    padding: 14px 16px;
-    background-color: inherit;
-    font-family: inherit;
-    margin: 0;
-}
-
-.dropdown-content {
-    display: none;
-    position: absolute;
-    background-color: #e0e0e0;
-    min-width: 160px;
-    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-    z-index: 1;
-}
-
-.dropdown-content a {
-    float: none;
-    color: black;
-    padding: 12px 16px;
-    text-decoration: none;
-    display: block;
-    text-align: left;
-}
-
-.topnav a:hover, .dropdown:hover .dropbtn {
-  background-color: inherit;
-  color: white;
-}
-
-.dropdown-content a:hover {
-    background-color: #959595;
-    color: black;
-}
-
-.dropdown:hover .dropdown-content {
-    display: block;
-}
-
-@media screen and (max-width: 480px) {
-  .topnav a:not(:first-child), .dropdown .dropbtn {
-    display: none;
-  }
-  .topnav a.icon {
-    float: left;
-    display: block;
-  }
-}
-
-@media screen and (max-width: 480px) {
-  .topnav.responsive {position: relative; padding-top:3em;}
-  .topnav.responsive .icon {
-    position: absolute;
-    left: 0;
-    top: 0;
-  }
-  .topnav.responsive a {
-    float: none;
-    display: block;
-    text-align: left;
-  }
-  .topnav.responsive .dropdown {float: none;}
-  .topnav.responsive .dropdown-content {position: relative;}
-  .topnav.responsive .dropdown .dropbtn {
-    display: block;
-    width: 100%;
-    text-align: left;
-  }
-}
-</style>
-<script src="./static/scripts/sweetalert2.min.js"></script>
-  <link rel="stylesheet" href="./static/scripts/sweetalert2.min.css">
-</head>
-<body>
+<title>Spotamon</title>
+<link rel="icon" href="static/img/favicon.ico" type="image/ico" sizes="16x16">
 
 <?php if ($disablemotd == true) {} elseif ($motdalways == true) {?>
-
 <script>swal({
   title: '<?php echo $motdtitle;?>',
   text: '<?php echo $motdtext;?>',
@@ -138,66 +19,75 @@ body {
   imageUrl: '<?php echo $motdimage;?>'
 })</script><?php }?><?php }?>
 
-<a href="./"><img src="header.png" alt="logo" style="padding-top:7px; float:left; background-color:#333; position:absolute; z-index:100;"></a>
-<div class="topnav" id="myTopnav">
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 
-	<div class="dropdown">
-    <button class="dropbtn"><a href="./" style="padding:0px !important;">Home</a></button>
-	</div>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-   <div class="dropdown">
-    <button class="dropbtn">Add spot <i class="fa fa-caret-down"></i></button>
-    <div class="dropdown-content">
-      <a href="./submit-pokemon.php" class="active">Pokémon</a>
-      <a href="./submit-raid.php">Raid</a>
-      <a href="./submit-team.php">Team</a>
-	  <a href="./submit-egg.php">Egg</a>
-	  <a href="./submit-quest.php">Quest</a>
+<script src="./static/scripts/sweetalert2.min.js"></script>
+<link rel="stylesheet" href="./static/scripts/sweetalert2.min.css">
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="style.css"><link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+<body>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" style="float:left; margin-left:10px;"class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
     </div>
-  </div>
-  <div class="dropdown">
-    <button class="dropbtn">View spots <i class="fa fa-caret-down"></i></button>
-    <div class="dropdown-content">
-      <a href="./pokemon.php">Pokémon</a>
-      <a href="./raids.php">Raids</a>
-	  <a href="./eggs.php">Eggs</a>
-	  <a href="./quests.php">Quests</a>
-    </div>
-  </div>
-  
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <ul class="nav navbar-nav">
+      <li><a href="./">Home</a></li>
+      <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Add spot
+        <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="./submit-pokemon.php">Pokémon</a></li>
+          <li><a href="./submit-raid.php">Raid</a></li>
+          <li><a href="./submit-team.php">Team</a></li>
+          <li><a href="./submit-egg.php">Egg</a></li>
+          <li><a href="./submit-quest.php">Quest</a></li>
+        </ul>
+      </li>
+      <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#">View spots
+        <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="./pokemon.php">Pokémon</a></li>
+          <li><a href="./raids.php">Raid</a></li>
+          <li><a href="./eggs.php">Egg</a></li>
+          <li><a href="./quests.php">Quest</a></li>
+        </ul>
+      </li>
+    </ul>
+      <ul class="nav navbar-nav navbar-right">
 	<?php
 	if(isset($_SESSION["uname"])){
 	?>
-	<div class="dropdown">
-    <button class="dropbtn">Welcome <?php echo $_SESSION['uname']; ?> <i class="fa fa-caret-down"></i></button>
-	 <div class="dropdown-content">
-	 <a href="profile.php">Profile</a>
-	 <a href="login/logout.php">Logout</a>
-	</div>
-	</div>
-	<?php } 
-	else{?>
-	<div class="dropdown">
-    <button class="dropbtn">Registration<i class="fa fa-caret-down"></i></button>
-	 <div class="dropdown-content">
-	 <a href="login/login.php">Login</a>
-	 <a href="login/registration.php">Register</a>
-	</div>
-	</div>
+        <li><a href="profile.php"><span class="glyphicon glyphicon-user"></span> Welcome <?php echo $_SESSION['uname']; ?></a></li>
+        <li><a href="/login/logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+	<?php } else {?>
+        <li><a href="/login/login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+        <li><a href="/login/registration.php"><span class="glyphicon glyphicon-user"></span> Register</a></li>
 	<?php }?>
-  
-  
-  <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
-</div>
-
-<script>
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
-</script></body>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+</body>
 <?php } ?>
--- index.php
+++ index.php
@@ -5,7 +5,6 @@ include './config/dbbuilding.php';
 ?>
 
 <head>
-	<link rel="stylesheet" type="text/css" href="style.css">
 	<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">

7059796d071695cedde32ed6aa4691c202232ac1
darkelement1987 <jgernaat1987@gmail.com>
2018-06-10 16:56:24 +0200

    Update update.php

--- update.php
+++ update.php
@@ -14,18 +14,5 @@ $addiv = "ALTER TABLE `spots` ADD `iv` INT(3) NOT NULL AFTER `cp`;";
 			}
 					
     header("Refresh: 3; url= index.php");
-	
-$addpics = "ALTER TABLE `users` ADD `url` TEXT NOT NULL AFTER `trn_date`, ADD `lastUpload` VARCHAR(200) NOT NULL AFTER `url`;";
-	if(!mysqli_query($conn,$addpics))
-		{
-			echo 'Not updated, profile pic columns already exist :-)';
-		}
-			else
-			{
-				echo 'Updating database...';
-			}
-					
-    header("Refresh: 3; url= index.php");
-
 ?>
 

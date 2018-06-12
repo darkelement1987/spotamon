eb3ca048d739aa859117f6c0811321da93190f94
lilcezz <35689207+lilcezz@users.noreply.github.com>
2018-06-11 22:34:48 -0400

    Add files via upload

--- spotexraid.php
+++ spotexraid.php
@@ -17,15 +17,65 @@ $sql1 = "UPDATE gyms SET exraid='1',exraiddate='$exraiddate' WHERE gid='$gname'"
             {
                 echo 'Inserted';
             }
-$sql = "SELECT * FROM gyms WHERE gid='$gname'";
-    if(!mysqli_query($conn,$sql1))
-        {
-            echo 'Not Inserted';
-        }
-            else
-            {
-                echo 'Inserted';
-            }
+
+	$gymquery = "SELECT gname,glatitude,glongitude FROM gyms WHERE gid = '$gname'";
+	if(!mysqli_query($conn,$gymquery))
+		{
+			echo 'Not Inserted';
+		}
+			else
+			{
+				echo 'Inserted';
+			}
+
+$resultgym = $conn->query($gymquery);
+
+$row = $resultgym->fetch_array(MYSQLI_NUM);
+$gymname = $row[0];
+$gymlat = $row[1];
+$gymlon = $row[2];
+		
+	$siteurl = "[".$viewtitle."](".$viewurl."/?loc=$gymlat,$gymlon&zoom=19)";
+		
+		$hookObject = json_encode([
+    "username" => "Raid Spotted!",
+    "tts" => false,
+	"avatar_url" => "$viewurl/static/raids/$rboss.png",
+    "embeds" => [
+        [
+            "type" => "rich",
+            "description" => "$siteurl",
+            "color" => hexdec( "FFFFFF" ),
+            "image" => [
+				"url" => "https://maps.googleapis.com/maps/api/staticmap?center=$gymlat,$gymlon&markers=$gymlat,$gymlon&zoom=17&size=400x400",
+            ],            
+            "fields" => [
+                [
+                    "name" => "Gym",
+                    "value" => "$gymname",
+                    "inline" => true
+                ]
+            ]
+        ]
+    ]
+    
+], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE );
+
+$ch = curl_init();
+
+curl_setopt_array( $ch, [
+    CURLOPT_URL => $webhook_url,
+    CURLOPT_POST => true,
+    CURLOPT_POSTFIELDS => $hookObject,
+    CURLOPT_HTTPHEADER => [
+        "Length" => strlen( $hookObject ),
+        "Content-Type" => "application/json"
+    ]
+]);
+
+$response = curl_exec( $ch );
+curl_close( $ch );
+			
     header('Location:index.php?loc='.$gymlat.','.$gymlon.'&zoom=19');
 
 ?>

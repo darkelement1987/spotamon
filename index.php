1fd0ec47ef2d791bb2fa9facc3565e20be39f48a
RedFirebreak <sjilderda@gmail.com>
2018-06-14 20:25:07 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include './config/dbbuilding.php';
 require './config/config.php';
 ?>
 
-<head onload="rndqu(n)">
+<head>
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
     <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">

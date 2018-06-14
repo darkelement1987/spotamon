b7718488908493631476f4ea7c15bacbc4587a25
RedFirebreak <sjilderda@gmail.com>
2018-06-14 20:25:39 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,6 @@ require './config/config.php';
     <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://cdn.klokantech.com/maptilerlayer/v1/index.js"></script>
-
     <script>
 
         function submitInstinct(){

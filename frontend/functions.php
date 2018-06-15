ef7bad4dd785a34243c04cd0d825d3dfb9d697ac
RedFirebreak <sjilderda@gmail.com>
2018-06-15 16:51:50 +0200

    Confirm password (#60)
    * Add files via upload
    
    * Add files via upload
    
    * Reg form upgrade
    
    Adds a "confirm password"
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    Somehow got wrong changes, my bad. Changed back to current index.php
    
    * Delete ultra-ball.png
    
    * Add files via upload
    
    * Add files via upload
    
    * Update registration.php
    
    <br><br>

--- frontend/functions.php
+++ frontend/functions.php
@@ -1241,13 +1241,55 @@ $id = $usergroup = "";?>
 	<?php echo "<th style='background-color:#f9f9f9;color:#000;'><center>Email: </center></th>";?>
 	<?php echo "<td><center><input type='text' name='email' id='email' pattern=\"[a-zA-Z0-9!#$%&amp;'*+\/=?^_`{|}~.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\"><input type='submit' value='Submit'></center></td></form>";?>
 	</tr>
-	
+
 	<tr>
 	<form action="editpassword.php" method="post">
-	<?php echo "<th style='background-color:#fff;color:#000;'><center>Password: </center></th>";?>
-	<?php echo "<td><center><input type='password' name='upass' id='upass'><input type='submit' value='Submit'></center></td></form>";?>
+	<?php echo "<th style='background-color:#fff;color:#000;'><center>Password: </center><br><br></th>";?>
+    <?php echo "<td><center><span id='message'></span><div id=\"error-nwl\"></div><input type=\"password\" minlength=\"6\" name=\"password\" id=\"password\" placeholder=\"password\" onkeydown=\"\" onkeyup=\"checkPass(); return false;\"/><input type=\"password\" minlength=\"6\" name=\"confirm_password\" id=\"confirm_password\" placeholder=\"confirm password\" onkeydown=\"\" onkeyup=\"checkPass(); return false;\" /><input type=\"submit\" name=\"submit\"  value=\"registration\"  id=\"submit\"/></td></form>";?>
 	</tr>
-	<?php echo "</table>";?>
+        <script>
+            $('input[type="submit"]').attr('disabled','disabled');
+            function checkPass()
+            {
+                var pass1 = document.getElementById('password');
+                var pass2 = document.getElementById('confirm_password');
+                var message = document.getElementById('error-nwl');
+                var goodColor = "#66cc66";
+                var badColor = "#ff6666";
+
+                if(pass1.value.length > 5)
+                {
+                    pass1.style.backgroundColor = goodColor;
+                    message.style.color = goodColor;
+                    $('input[type="submit"]').attr('disabled','disabled');
+                    message.innerHTML = "character number ok!"
+                }
+                else
+                {
+                    pass1.style.backgroundColor = badColor;
+                    message.style.color = badColor;
+                    $('input[type="submit"]').attr('disabled','disabled');
+                    message.innerHTML = "<br>You have to enter at least 6 digit!"
+                    return;
+                }
+
+                if(pass1.value == pass2.value)
+                {
+                    pass2.style.backgroundColor = goodColor;
+                    message.style.color = goodColor;
+                    message.innerHTML = "<br>Ready to go!"
+                    $('input[type="submit"]').removeAttr('disabled');
+                }
+                else
+                {
+                    pass2.style.backgroundColor = badColor;
+                    message.style.color = badColor;
+                    $('input[type="submit"]').attr('disabled','disabled');
+                    message.innerHTML = "<br>These passwords don't match!"
+                }
+            }
+        </script>
+        <?php echo "</table>";?>
 	<h2 style="text-align:center;"><strong>Upload profile picture:</strong></h2>
 	<?php
 	}
--- index.php
+++ index.php
@@ -6,45 +6,45 @@ require './config/config.php';
 ?>
 
 <head>
-	<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
-	<meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<script src="https://cdn.klokantech.com/maptilerlayer/v1/index.js"></script>
-<script>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://cdn.klokantech.com/maptilerlayer/v1/index.js"></script>
+    <script>
 
-function submitInstinct(){
-	document.postInstinct.submit();
-	}
-	function submitValor(){
-		document.postValor.submit();
-		}
-		function submitMystic(){
-			document.postMystic.submit();
-			}
-			</script>
+        function submitInstinct(){
+            document.postInstinct.submit();
+        }
+        function submitValor(){
+            document.postValor.submit();
+        }
+        function submitMystic(){
+            document.postMystic.submit();
+        }
+    </script>
 
-<?php
-menu();
+    <?php
+    menu();
 
-maps();
+    maps();
 
-?>
+    ?>
 
-<?php
-// Update map if needed
-require './config/version.php';
-$versionquery = "SELECT version FROM version";
-$versionresult = $conn->query($versionquery);
-$rowversion = $versionresult->fetch_array(MYSQLI_NUM);
-$version = $rowversion[0];	
+    <?php
+    // Update map if needed
+    require './config/version.php';
+    $versionquery = "SELECT version FROM version";
+    $versionresult = $conn->query($versionquery);
+    $rowversion = $versionresult->fetch_array(MYSQLI_NUM);
+    $version = $rowversion[0];
 
-if ($version =='') {
-	$conn->query("INSERT IGNORE INTO `version` (`version`) VALUES ('1')");
-} else if ($version < $lastversion) {
-	$conn->query("UPDATE version SET version='".$lastversion."'");
-	echo "<meta http-equiv='refresh' content='1;url=update.php'>";
-}
-?>
+    if ($version =='') {
+        $conn->query("INSERT IGNORE INTO `version` (`version`) VALUES ('1')");
+    } else if ($version < $lastversion) {
+        $conn->query("UPDATE version SET version='".$lastversion."'");
+        echo "<meta http-equiv='refresh' content='1;url=update.php'>";
+    }
+    ?>
 
-<footer></footer>
+    <footer></footer>
 

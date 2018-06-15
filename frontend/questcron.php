cd1d27b5d714d2b3e21edd0096ca229f57bdca2a
AboveColin <colindv2000@gmail.com>
2018-06-15 09:57:55 +0200

    Update Questcron.php
    Quests are automatically changed after 00:00 not 24 hours after it was submitted @ spotamon

--- frontend/questcron.php
+++ frontend/questcron.php
@@ -1,6 +1,6 @@
 <?php
 require '../config/config.php';
-$sql = "UPDATE stops SET quested='0',actquest='0',actreward='0',hour='0', min='0',ampm='0',questby='0' WHERE date < (NOW() - INTERVAL 24 HOUR)";
+$sql = "UPDATE stops SET quested='0',actquest='0',actreward='0',hour='0', min='0',ampm='0',questby='0' WHERE date < CURDATE()";
 if(!mysqli_query($conn,$sql))
 {
     echo 'Not Deleted';

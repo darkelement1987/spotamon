2563705ff9b790ceb2b39854669c9aa6b4c32c16
RedFirebreak <sjilderda@gmail.com>
2018-06-15 10:45:05 +0200

    Add files via upload
    Somehow got wrong changes, my bad. Changed back to current index.php

--- index.php
+++ index.php
@@ -10,44 +10,41 @@ require './config/config.php';
     <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://cdn.klokantech.com/maptilerlayer/v1/index.js"></script>
-    <meta property="og:title" content="Spotamon, Pokemon in your area">
-    <meta property="og:description" content="spot a pokemon, raid, pokestop quest or more all in your local area! Welcome to spotamon">
-    <meta property="og:image" content="/static/img/ultra-ball.png">
     <script>
 
-function submitInstinct(){
-	document.postInstinct.submit();
-	}
-	function submitValor(){
-		document.postValor.submit();
-		}
-		function submitMystic(){
-			document.postMystic.submit();
-			}
-			</script>
+        function submitInstinct(){
+            document.postInstinct.submit();
+        }
+        function submitValor(){
+            document.postValor.submit();
+        }
+        function submitMystic(){
+            document.postMystic.submit();
+        }
+    </script>
 
-<?php
-menu();
+    <?php
+    menu();
 
-maps();
+    maps();
 
-?>
+    ?>
 
-<?php
-// Update map if needed
-require './config/version.php';
-$versionquery = "SELECT version FROM version";
-$versionresult = $conn->query($versionquery);
-$rowversion = $versionresult->fetch_array(MYSQLI_NUM);
-$version = $rowversion[0];	
+    <?php
+    // Update map if needed
+    require './config/version.php';
+    $versionquery = "SELECT version FROM version";
+    $versionresult = $conn->query($versionquery);
+    $rowversion = $versionresult->fetch_array(MYSQLI_NUM);
+    $version = $rowversion[0];
 
-if ($version =='') {
-	$conn->query("INSERT IGNORE INTO `version` (`version`) VALUES ('1')");
-} else if ($version < $lastversion) {
-	$conn->query("UPDATE version SET version='".$lastversion."'");
-	echo "<meta http-equiv='refresh' content='1;url=update.php'>";
-}
-?>
+    if ($version =='') {
+        $conn->query("INSERT IGNORE INTO `version` (`version`) VALUES ('1')");
+    } else if ($version < $lastversion) {
+        $conn->query("UPDATE version SET version='".$lastversion."'");
+        echo "<meta http-equiv='refresh' content='1;url=update.php'>";
+    }
+    ?>
 
-<footer></footer>
+    <footer></footer>
 

6d990eee8c2dee660d8c03604102fd0ec63f2381
AboveColin <colindv2000@gmail.com>
2018-06-23 14:24:29 +0200

    Update login.php

--- login/login.php
+++ login/login.php
@@ -1,8 +1,25 @@
 <?php
 	require('../config/config.php');
 	session_start();
+	$post_data = http_build_query(
+    array(
+        'secret' => $captcha_secret_key,
+        'response' => $_POST['g-recaptcha-response'],
+        'remoteip' => $_SERVER['REMOTE_ADDR']
+    )
+);
+$opts = array('http' =>
+    array(
+        'method'  => 'POST',
+        'header'  => 'Content-type: application/x-www-form-urlencoded',
+        'content' => $post_data
+    )
+);
+$context  = stream_context_create($opts);
+$response = file_get_contents('https://www.google.com/recaptcha/api/siteverify', false, $context);
+$result = json_decode($response);
     // If form submitted, insert values into the database.
-    if (isset($_POST['uname'])){
+    if ($result->success && isset($_POST['uname'])){
 		
 		$uname = stripslashes($_REQUEST['uname']); // removes backslashes
 		$uname = mysqli_real_escape_string($conn,$uname); //escapes special characters in a string
@@ -27,6 +44,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 <meta name="viewport" content="width=device-width">
 <link rel="stylesheet" href="./css/style.css" />
+<script src='https://www.google.com/recaptcha/api.js'></script>
 </head>
 <body>
 <center>
@@ -45,6 +63,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 <meta name="viewport" content="width=device-width">
 <link rel="stylesheet" href="./css/style.css" />
+<script src='https://www.google.com/recaptcha/api.js'></script>
 </head>
 <body>
 <center>
@@ -53,6 +72,7 @@
 <form action="" method="post" name="login">
 <p><input type="text" name="uname" placeholder="Username" required /></p>
 <p><input type="password" name="upass" placeholder="Password" required /></p>
+<p><div class="g-recaptcha" data-sitekey=<?php echo $captcha_site_key; ?>></div></p>
 <p><input name="submit" type="submit" value="Login" /></p>
 <p><a href="../reset.php">Forgot password</a></p>
 </form>

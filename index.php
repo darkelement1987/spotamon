f2ed9e3d7d1b2d2ff423d19d5751880593c8911f
darkelement1987 <jgernaat1987@gmail.com>
2018-07-09 12:53:47 +0200

    Important Bugfix 2

--- index.php
+++ index.php
@@ -9,7 +9,6 @@ require './config/config.php';
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
     <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="https://cdn.klokantech.com/maptilerlayer/v1/index.js"></script>
     <script>
 
         function submitInstinct(){

c827c03d383d4c4268dcdf85bb209d0235448d7e
darkelement1987 <jgernaat1987@gmail.com>
2018-07-09 12:52:28 +0200

    Important bugfix

--- index.php
+++ index.php
@@ -2,12 +2,14 @@
 include './frontend/functions.php';
 include './frontend/menu.php';
 include './config/dbbuilding.php';
+require './config/config.php';
 ?>
 
 <head>
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
     <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://cdn.klokantech.com/maptilerlayer/v1/index.js"></script>
     <script>
 
         function submitInstinct(){
